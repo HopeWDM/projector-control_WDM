5a4d8dfe7ad887864c71ae1f2f0b7907e73fcdbc
willpile <willpile@users.noreply.github.com>
2017-10-12 10:17:33 -0500

    Update common_pitron-stuff.js

--- pitron_WDM/javascript/common_pitron-stuff.js
+++ pitron_WDM/javascript/common_pitron-stuff.js
@@ -227,7 +227,7 @@ var deviceType;
 
 function pitronBox (address, deviceType, action)
 {
-
+	//this is where we input the individual IP address for each pitron
 	var address = 'http://10.40.30.81/cgi-bin/cgi.pl'
 	//var urlStart = address + '?cmd=W' + port + 'RS|';
 	var urlStart = address + '?cmd=' ;
