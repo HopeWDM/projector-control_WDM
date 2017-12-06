bf537977cd18ed30bce7350f4cae7062c1a584e4
cf8geek <cf8geek@users.noreply.github.com>
2017-12-05 20:34:06 -0600

    Update common_pitron-stuff.js

--- room-controls/javascript/common_pitron-stuff.js
+++ room-controls/javascript/common_pitron-stuff.js
@@ -163,6 +163,14 @@ actions that are supported on at least one device:
 	input-HDMI
 	input-SVid
 	input-VGA
+	input-1
+	input-2
+	input-3
+	input-4
+	input-5
+	input-6
+	input-7
+	input-8
 	keystone-down
 	keystone-left
 	keystone-right
